4d1d757258722f0f9ef79fb13102ccc2729f009f
KienLe-git <kien.letrung610@gmail.com>
2021-12-13 19:08:26 +0700

    fix level 3

--- algorithms/stockfish/stockfish_bot.py
+++ algorithms/stockfish/stockfish_bot.py
@@ -9,13 +9,13 @@ if platform == "linux" or platform == "linux2":
     os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_linux_x64')
     engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_linux_x64")
 elif platform == "win32":
-    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_32bit.exe')
+    # os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_32bit.exe')
     engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_win_32bit.exe")
 elif platform == "win64":
-    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_x64.exe')
+    # os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_x64.exe')
     engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_win_x64.exe")
-# elif platform == "darwin":
-#     # MAC OS X
+elif platform == "darwin":
+    print("Don't support level 3")
 
 # %%
 def find_move(board):
