53737752a35bfd6fd10ba113f117d865325e54c9
KienLe-git <kien.letrung610@gmail.com>
2021-12-13 18:04:57 +0700

    stockfish linux, win32, 64

--- algorithms/stockfish/stockfish_bot.py
+++ algorithms/stockfish/stockfish_bot.py
@@ -6,13 +6,14 @@ from sys import platform as platform
 
 # %%
 if platform == "linux" or platform == "linux2":
-    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_linux_x64/stockfish_14.1_linux_x64')
-    engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_linux_x64/stockfish_14.1_linux_x64")
-
-
-# elif platform == "win32":
-#     # Windows
-# elif platform == "win64":
+    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_linux_x64')
+    engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_linux_x64")
+elif platform == "win32":
+    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_32bit.exe')
+    engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_win_32bit.exe")
+elif platform == "win64":
+    os.system('chmod +x ./algorithms/stockfish/stockfish_14.1_win_x64.exe')
+    engine = chess.engine.SimpleEngine.popen_uci(r"./algorithms/stockfish/stockfish_14.1_win_x64.exe")
 # elif platform == "darwin":
 #     # MAC OS X
 
